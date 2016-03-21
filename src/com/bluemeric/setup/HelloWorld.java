68344fdc39aa7caafcf5d259f1dd596869cef6cc
Vidhya <vidhya.n@trov.co.in>
2016-03-21 20:40:38 +0530

    For testing

--- src/com/bluemeric/setup/HelloWorld.java
+++ src/com/bluemeric/setup/HelloWorld.java
@@ -36,7 +36,7 @@ public class HelloWorld {
   @Produces(MediaType.TEXT_HTML)
   public String sayHelloWorld2() {
 	  return "<html> " + "<title>" + "Hello World" + "</title>"
-		        + "<body><h1>" + "Hello World! Welcome to Bluemeric Demo13!" + "</body></h1>" + "</html> ";
+		        + "<body><h1>" + "Hello World! Welcome to Bluemeric Demo1!" + "</body></h1>" + "</html> ";
   }
   
   @GET
