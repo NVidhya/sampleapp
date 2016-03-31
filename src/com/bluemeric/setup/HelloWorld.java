f4e317a214fd29b707921059f652cc6c99013591
Vidhya <vidhya.n@trov.co.in>
2016-03-31 07:46:13 +0530

    Removed unwanted methods from helloworld

--- src/com/bluemeric/setup/HelloWorld.java
+++ src/com/bluemeric/setup/HelloWorld.java
@@ -9,34 +9,17 @@ import javax.ws.rs.core.MediaType;
 @Path("/")
 public class HelloWorld {
 
-
   @GET
   @Path("/helloworld")
   @Produces(MediaType.TEXT_PLAIN)
   public String sayHelloWorld() {
-    return "Hello World";
-  }
+    return "Hello World" ;
+  } 
 
-  @GET
-  @Path("/helloworld0")
-  @Produces(MediaType.TEXT_XML)
-  public String sayHelloWorld0() {
-    return "<?xml version=\"1.0\"?>" + "<hello> Hello World" + "</hello>";
-  }
-
-  
   @GET
   @Path("/helloworld1")
-  @Produces(MediaType.TEXT_HTML)
-  public String sayHelloWorld2() {
-	  return "<html> " + "<title>" + "Hello World" + "</title>"
-		        + "<body><h1>" + "Hello World! Welcome to Bluemeric Demo!" + "</body></h1>" + "</html> ";
-  }
-  
-  @GET
-  @Path("/helloworld2")
   @Produces(MediaType.TEXT_PLAIN)
-  public String sayHelloWorld3 (@QueryParam("name") String name) {
-	  return "Hello World " + name;
-  }
+  public String sayHelloWorld1(@QueryParam("name") String name) {
+    return "Hello World " + name ;
+  } 
 } 
